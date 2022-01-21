dd0fd594028c8467e686ea5e7fa178c8e457936e
HJH <nahjh2@gmail.com>
2022-01-21 12:58:24 +0900

    fin

--- config/settings.py
+++ config/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     # my apps
     'pybo.apps.PyboConfig',
+    'common.apps.CommonConfig',  
 
     # default apps
     'django.contrib.admin',
@@ -128,3 +129,9 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# 로그인 성공후 이동하는 URL
+LOGIN_REDIRECT_URL = '/'
+
+# 로그아웃시 이동하는 URL
+LOGOUT_REDIRECT_URL = '/'
---
+++ pybo/views/answer_views.py
@@ -0,0 +1,38 @@
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+
+from ..forms import AnswerForm
+from ..models import Question, Answer
+
+
+@login_required(login_url='common:login')
+def answer_create(request, question_id):
+    """
+    pybo 답변등록
+    """
+
+    question = get_object_or_404(Question, pk=question_id)
+    # question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    # # answer = Answer(question=question, content=request.POST.get('content'), create_date=timezone.now())
+    # # answer.save()
+
+    if request.method == "POST":
+        form = AnswerForm(request.POST)
+
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.author = request.user  # author 속성에 로그인 계정 저장
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+
+            return redirect('pybo:detail', question_id=question.id)
+
+    else:
+        form = AnswerForm()
+
+    context = {'question': question, 'form': form}
+
+    return render(request, 'pybo/question_detail.html', context)  
